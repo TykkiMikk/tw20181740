92e1233df49024bd5a79b695816db8d94812a042
TykkiMikk <973999906@qq.com>
2018-06-09 11:58:02 +0800

    init

---
+++ src/main/java/DateUtil.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class DateUtil {
+    public static Boolean dateIsEqual(Date dt1, Date dt2) {
+        return true;
+    }
+}
---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class OrderTest {
+    @Test
+    public void shouldReturnIsEqual() throws ParseException {
+        SimpleDateFormat simFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        Date dtBeg = simFormat.parse("2008.01.23 22:45:56");
+        Date dtBeg2 = simFormat.parse("2008.01.23 22:45:56");
+
+        assertEquals(DateUtil.dateIsEqual(dtBeg, dtBeg2), true);
+    }
+}
